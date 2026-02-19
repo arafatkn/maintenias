82a1a417f33bfa3c3fb7c34bb4885dd7c8331688
Arafat Islam <arafatkn@gmail.com>
2026-02-19 18:54:54 +0600

    making ready for release

--- maintenias.php
+++ maintenias.php
@@ -23,7 +23,6 @@
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: maintenias
- * Domain Path: /languages
  */
 
 // If this file is called directly, abort.
--- templates/app.php
+++ templates/app.php
@@ -1,3 +1,4 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 <div id="maintenias">
 	<h2 style="text-align: center; margin-top: 20px"><?php esc_html_e( 'Loading', 'maintenias' ); ?>...</h2>
 </div>
